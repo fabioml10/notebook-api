1b691a0f256903707f38bb82524b99564f8fcace
Fabio Muller <fabioml10@yahoo.com.br>
2020-08-21 18:23:32 -0300

    CRUD on serializers routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
     resource :phones, only: [:show]
     resource :phones, only: [:show], path: 'relationships/phones'
 
-    resource :address, only: [:show]
-    resource :address, only: [:show], path: 'relationships/address'
+    resource :phone, only: [:update, :create, :destroy]
+    resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+
+    resource :address, only: [:show, :update, :create, :destroy]
+    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
   end
 end
